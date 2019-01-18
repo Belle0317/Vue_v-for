7ed876d4220c19c97f09aba490222e751183ba28
Belle0317 <46584373+Belle0317@users.noreply.github.com>
2019-01-18 18:35:46 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		foodList: [{
+			name: '葱',
+			price: '10',
+			discount: '.8',
+		}, {
+			name: '姜',
+			price: '20',
+			discount: '.8',
+		}, {
+			name: '蒜',
+			price: '5',
+		}, ],
+	},
+
+});
